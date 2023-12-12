4e037e74179e95271cd605b2ed1e1dd9236f4ff8
Mateichuk Mykola <mateichukmykola@gmail.com>
2023-12-12 23:32:09 +0200

    Polish admin page

--- theatre/admin.py
+++ theatre/admin.py
@@ -8,5 +8,14 @@ admin.site.register(Genre)
 admin.site.register(Play)
 admin.site.register(TheatreHall)
 admin.site.register(Performance)
-admin.site.register(Reservation)
 admin.site.register(Ticket)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    inlines = [TicketInline]
